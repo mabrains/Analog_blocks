365563e0fcaa5e66d3ce3e3b7a99825535033cf2
EslamMorsie <eslam_morsie@contractor.mabrains.com>
2021-06-10 17:47:38 +0200

    OTA layout added

--- scripts/OTA_script.bash
+++ scripts/OTA_script.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/Analog_blocks/Analog_Blocks/OTA/Netlists/Testbench
-ngspice -i "Miller_OTA1.8v_meas.spice" -a || sh
-ngspice -i "Miller_OTA1.8v_meas_transient.spice" -a || sh
+#ngspice -i "Miller_OTA5v_meas.spice" -a || sh
+ngspice -i "Miller_OTA5v_meas_post.spice" -a || sh
 
